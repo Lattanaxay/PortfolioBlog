b9dabbd4bf1906954de7b5d2f6f200d1a8cb4567
Lattanaxay <Lattanaxaycaster@gmail.com>
2023-08-25 00:54:08 +0800

    Portfolio Section JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,4 +10,14 @@ let mixerProjects = mixitup('.projects__container', {
         duration: 300
     }
 });
+
+/* active work */
+const linkWork = document.querySelectorAll('.category__btn');
+
+function activeWork(){
+    linkWork.forEach((a)=> a.classList.remove('.active-work'));
+    this.classList.add('active-work');
+}
+
+linkWork.forEach((a)=> a.addEventListener('click', activeWork));
 /*=============== Testimonials Swiper =============== */
