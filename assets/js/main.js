0daf7b0bae4365253579b93415d11d3e3ed4c118
Lattanaxay <Lattanaxaycaster@gmail.com>
2023-08-24 23:20:33 +0800

    project blog

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,13 @@
+'use strict';
 /*=============== Active Link =============== */
 
 /*=============== Mixitup Filter =============== */
-
+let mixerProjects = mixitup('.projects__container', {
+    selectors: {
+        target: '.blog-item'
+    },
+    animation: {
+        duration: 300
+    }
+});
 /*=============== Testimonials Swiper =============== */
