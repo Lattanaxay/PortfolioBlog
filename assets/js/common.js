a8c53d362d02feb468ef0460def4f09484170722
Lattanaxay <Lattanaxaycaster@gmail.com>
2023-09-01 00:40:54 +0800

    Menu Toggle Remove Menu Mobile but still have some problem whit the css

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,13 +1,33 @@
 /*=============== Show Menu =============== */
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
+
 
 /*===== Menu Show =====*/
 /* Validate if constant exists */
-
+if(navToggle) {
+  navToggle.addEventListener('click',()=>{
+    navMenu.classList.add('show-menu');
+  });
+}
 /*===== Hide Show =====*/
 /* Validate if constant exists */
-
+if(navClose) {
+  navClose.addEventListener('click',()=>{
+    navMenu.classList.remove('show-menu');
+  });
+}
 /*=============== Remove Menu Mobile =============== */
+const navLink = document.querySelectorAll('.nav__link');
 
+function linkAction() {
+  const navMenu = document.getElementById('nav-menu');
+  //when we click on each link, we remove the show-menu class
+  navMenu.classList.remove('show-menu');
+}
+
+navLink.classList.remove('show-menu');
 /*=============== Background Header =============== */
 function scrollHeader() {
     const header = document.getElementById('header')
